7b859b42caf13115aa890b15c5c9253913e51ebd
Matveev Timofey <justshowel@gmail.com>
2022-02-07 21:34:27 +0300

    feat: Hide control bar before video is launched

--- portfolio/modules/video.js
+++ portfolio/modules/video.js
@@ -18,7 +18,7 @@ function addCssLink(){
 }
 
 const controlsMarkup = `
-    <div class="video-controls">
+    <div class="video-controls hidden">
         <div class="control-group playback">
             <button class="custom-icons clickable video-play video-button"></button>
             <input class="video-range progress" type="range" min="0" max="100" step="0.5" value="0">
@@ -49,6 +49,7 @@ class VideoControl {
     volumeBar = null;
     playButton = null;
     bigPlayButton = null;
+    controls = null;
     audio = new Proxy({
         'muted': false,
         'currentVolume': 0,
@@ -64,8 +65,9 @@ class VideoControl {
         }
     });
 
-    constructor(video, progressBar, volumeBar, volumeButton, playButton, bigPlayButton) {
+    constructor(video, controls, progressBar, volumeBar, volumeButton, playButton, bigPlayButton) {
         this.video = video;
+        this.controls = controls;
         this.video.controls = false;
         this.progressBar = progressBar;
         this.volumeButton = volumeButton;
@@ -76,6 +78,7 @@ class VideoControl {
         this.audio.handlers['muted'].push(this.syncVolume, this.updateVolumeIcon);
 
         this.bigPlayButton.addEventListener('click', this.togglePlayback);
+        this.bigPlayButton.addEventListener('click', this.showControls, {'once': true});
         this.playButton.addEventListener('click', this.togglePlayback);
         this.volumeButton.addEventListener('click', this.mute);
         this.volumeBar.addEventListener('input', this.setVolume);
@@ -88,7 +91,11 @@ class VideoControl {
         this.video.addEventListener('timeupdate', this.updateProgress);
         this.video.addEventListener('play', this.updatePlayIcons);
         this.video.addEventListener('pause', this.updatePlayIcons);
-        this.video.addEventListener('loadedmetadata', event => this.progressBar.max = Math.floor(this.video.duration));
+        this.video.addEventListener('loadedmetadata', event => this.progressBar.max = Math.round(this.video.duration));
+    }
+
+    showControls = event => {
+        this.controls.classList.remove('hidden');
     }
 
     togglePlayback = event => {
@@ -124,7 +131,7 @@ class VideoControl {
     setVolume = event => {
         if (this.volumeBar.value !== event.target.value)
             this.volumeBar.value = event.target.value;
-        this.audio.muted = false;
+        this.audio.muted = event.target.value == 0;
         this.audio.currentVolume = event.target.value;
     }
 
@@ -154,7 +161,7 @@ export function setupCustomControls(videoContainer) {
     let playButton = videoContainer.querySelector(".video-play");
     let bigPlayButton = videoContainer.querySelector(".play-button");
     let volumeBar = videoContainer.querySelector(".video-range.volume");
-    let videoControl = new VideoControl(videoContainer.querySelector("video"), progress, volumeBar, volumeButton, playButton, bigPlayButton);
+    let videoControl = new VideoControl(videoContainer.querySelector("video"), videoContainer.querySelector('.video-controls'), progress, volumeBar, volumeButton, playButton, bigPlayButton);
     volumeBar.value = 0.5;
     volumeBar.dispatchEvent(new Event('input', {bubbles:true}));
     return videoControl;
